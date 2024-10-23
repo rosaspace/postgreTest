1982853ffb3a1f8916dd9e58b1b90ddf1325f506
rosaspace <rosaspace@126.com>
2024-10-23 00:08:21 -0500

    first commit

---
+++ postgre/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
